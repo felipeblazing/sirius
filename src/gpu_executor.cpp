b95c5f4d73dde09ce043b281259458321455bbb1
Yifei-yang7 <yyang673@wisc.edu>
2025-10-28 23:09:36 +0000

    Fall back regex expression to duckdb

--- src/gpu_executor.cpp
+++ src/gpu_executor.cpp
@@ -17,7 +17,7 @@
 #include "gpu_context.hpp"
 #include "duckdb/execution/operator/set/physical_recursive_cte.hpp"
 #include "duckdb/execution/operator/helper/physical_result_collector.hpp"
-#include "fallback_queries.hpp"
+#include "fallback.hpp"
 #include "gpu_physical_operator.hpp"
 #include "operator/gpu_physical_result_collector.hpp"
 #include "operator/gpu_physical_hash_join.hpp"
@@ -57,8 +57,11 @@ void GPUExecutor::Initialize(unique_ptr<GPUPhysicalOperator> plan) {
 }
 
 void GPUExecutor::Execute() {
-	check_fallback_queries(gpu_context.gpu_active_query->query);
+	// Check if we should fall back to duckdb execution.
+	FallbackChecker fallback_checker(scheduled);
+	fallback_checker.Check();
 
+	// Execution starts here.
 	int initial_idx = 0;
 
 	SIRIUS_LOG_DEBUG("Total meta pipelines {}", scheduled.size());
